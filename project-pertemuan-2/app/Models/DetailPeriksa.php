40f37d577b63c4caba3cf9fca5461f271b132eb9
MasahiroBenzSoeryo <mbsoeryo@gmail.com>
2025-03-23 23:38:24 +0700

    upload tugas pertemuan 2

---
+++ project-pertemuan-2/app/Models/DetailPeriksa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailPeriksa extends Model
+{
+    protected $fillable = [
+        'id_periksa',
+        'id_obat',
+    ];
+
+    public function detail()
+    {
+        return $this->belongsTo(Periksa::class, 'id_periksa');
+    }
+
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat');
+    }
+}
